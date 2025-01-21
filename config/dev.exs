2027df1a6fdaf83db952467c9e08d2262e894d60
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-01-21 21:56:32 +0530

    local db creds added to dev and test configs

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :pento, Pento.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "pento",
+  password: "pento",
   hostname: "localhost",
-  database: "pento_dev",
+  database: "pento",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
